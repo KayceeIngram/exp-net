c934f25ee7b6e69f77c1d1a3d9859fb1bb0627df
kayceeingram <ingramkaycee@gmail.com>
2020-01-23 20:56:02 -0600

    added

--- express/server.js
+++ express/server.js
@@ -1,9 +1,15 @@
 'use strict';
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+
+var corsOptions = {
+  origin: 'https://mcintire.netlify.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -11,8 +17,9 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/', cors(corsOptions), (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
